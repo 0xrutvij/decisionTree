8a5ad58ed3085cc45b449f5a64390a6c98a12981
Hallie-Nell <71307308+Hallie-Nell@users.noreply.github.com>
2021-02-27 03:50:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,92 +29,93 @@ for index, rows in df.iterrows():
     t = ax.trainingExample(label, my_list)
     trainingData.append(t)
 
-someTree = dt.decisionTree(trainingData, 2)
+# for depth = 1,...,10
+for x in range(1,11):
+    
+    someTree = dt.decisionTree(trainingData, x)
 
-someTree.train()
+    someTree.train()
 
-print(str(someTree))
+    print(str(someTree))
 
-#print(someTree.testSingle(lst[1]))
+    testResult = someTree.testBatch(trainingData)
 
-testResult = someTree.testBatch(trainingData)
+    numMiss = 0
+    numHit = 0
+    FN = TN = TP = FP = 0
 
-numMiss = 0
-numHit = 0
-FN = TN = TP = FP = 0
+    for x in testResult:
 
-for x in testResult:
-
-    if(x[2]):
-        numHit+=1
-        if(x[0]):
-            TP+=1
+        if(x[2]):
+            numHit+=1
+            if(x[0]):
+                TP+=1
+            else:
+                TN+=1
         else:
-            TN+=1
-    else:
-        numMiss+=1
-        if(x[0]):
-            FP+=1
+            numMiss+=1
+            if(x[0]):
+                FP+=1
+            else:
+                FN+=1
+
+    print('Error percent:', (numMiss*100)/(numMiss+numHit))
+
+
+    '''
+        PV ->
+    AV    _P_ _N_
+    | |P[TP][FN]      2|1
+    V |N[FP][TN]      3|4
+    '''
+
+    print(TP, '|', FN)
+    print(FP, '|', TN)
+
+    print('\n'*3)
+
+    df = pd.read_csv('./csv/monks-1.test.csv')
+
+
+    testData = []
+
+    for index, rows in df.iterrows():
+
+        my_list = [rows.f1, rows.f2, rows.f3, rows.f4, rows.f5, rows.f6,]
+        label = rows.label
+        t = ax.trainingExample(label, my_list)
+        testData.append(t)
+
+    testResult2 = someTree.testBatch(testData)
+
+    numMiss = 0
+    numHit = 0
+    FN = TN = TP = FP = 0
+
+    for x in testResult2:
+
+        if(x[2]):
+            numHit+=1
+            if(x[0]):
+                TP+=1
+            else:
+                TN+=1
         else:
-            FN+=1
+            numMiss+=1
+            if(x[0]):
+                FP+=1
+            else:
+                FN+=1
 
-print('Error percent:', (numMiss*100)/(numMiss+numHit))
-
-
-'''
-       PV ->
-AV    _P_ _N_
- | |P[TP][FN]      2|1
- V |N[FP][TN]      3|4
-'''
-
-print(TP, '|', FN)
-print(FP, '|', TN)
-
-print('\n'*3)
-
-df = pd.read_csv('./csv/monks-1.test.csv')
+    print('Error percent (test data):', (numMiss*100)/(numMiss+numHit))
 
 
-testData = []
+    '''
+        PV ->
+    AV    _P_ _N_
+    | |P[TP][FN]      2|1
+     V |N[FP][TN]      3|4
+    '''
 
-for index, rows in df.iterrows():
-
-    my_list = [rows.f1, rows.f2, rows.f3, rows.f4, rows.f5, rows.f6,]
-    label = rows.label
-    t = ax.trainingExample(label, my_list)
-    testData.append(t)
-
-testResult2 = someTree.testBatch(testData)
-
-numMiss = 0
-numHit = 0
-FN = TN = TP = FP = 0
-
-for x in testResult2:
-
-    if(x[2]):
-        numHit+=1
-        if(x[0]):
-            TP+=1
-        else:
-            TN+=1
-    else:
-        numMiss+=1
-        if(x[0]):
-            FP+=1
-        else:
-            FN+=1
-
-print('Error percent (test data):', (numMiss*100)/(numMiss+numHit))
-
-
-'''
-       PV ->
-AV    _P_ _N_
- | |P[TP][FN]      2|1
- V |N[FP][TN]      3|4
-'''
-
-print(TP, '|', FN)
-print(FP, '|', TN)
+    print(TP, '|', FN)
+    print(FP, '|', TN)
