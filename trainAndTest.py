dbb138134e7b363442ac369ac0d51d6a35e24bcb
Hallie-Nell <71307308+Hallie-Nell@users.noreply.github.com>
2021-02-27 10:28:39 -0600

    Update trainAndTest.py

--- trainAndTest.py
+++ trainAndTest.py
@@ -80,7 +80,7 @@ def trainAndTest(trainingData, x, testData, makeConfusion):
     print('Results for Training Data for Depth of', x, ':')
     trainingError = findResults(testResult, makeConfusion)
     
-    print('\n'*3)
+    print('\n')
     
     # find test error and make confusion matrix if needed
     testResult2 = someTree.testBatch(testData)
