e75f37216910f3fa809e4c222e09702feda5cbaa
Hallie-Nell <71307308+Hallie-Nell@users.noreply.github.com>
2021-02-27 05:16:34 -0600

    Update trainAndTest.py

--- trainAndTest.py
+++ trainAndTest.py
@@ -3,7 +3,7 @@ import decisionTree as dt
 import auxiliary as ax
 
 # function to create the training data
-def createTrainingData(trainingData):
+def createTrainingData(trainingData, df):
     for index, rows in df.iterrows():
 
         my_list = [rows.f1, rows.f2, rows.f3, rows.f4, rows.f5, rows.f6,]
@@ -14,7 +14,7 @@ def createTrainingData(trainingData):
     return trainingData
 
 # function to create the test data
-def createTestData(testData):
+def createTestData(testData, df):
     
     for index, rows in df.iterrows():
         my_list = [rows.f1, rows.f2, rows.f3, rows.f4, rows.f5, rows.f6,]
@@ -24,7 +24,9 @@ def createTestData(testData):
     
     return testData 
 
-def findPosNegValues(testResult):
+# function to compute the training and test results. If makeConfusion = 1, then 
+# a confusion matrix will be produced
+def findResults(testResult, makeConfusion):
     
     numMiss = 0
     numHit = 0
@@ -45,65 +47,42 @@ def findPosNegValues(testResult):
             else:
                 FN+=1
 
-    print('Training Error Percent:', (numMiss*100)/(numMiss+numHit))
+    print('Error',(numMiss*100)/(numMiss+numHit))
 
+    # If makeConfusion = 1, make a confusion matrix
+    if makeConfusion==1:
+        '''
+            PV ->
+        AV    _P_ _N_
+        | |P[TP][FN]      2|1
+        V |N[FP][TN]      3|4
+        '''
 
-    '''
-        PV ->
-    AV    _P_ _N_
-    | |P[TP][FN]      2|1
-    V |N[FP][TN]      3|4
-    '''
+        print(TP, '|', FN)
+        print(FP, '|', TN)
 
-    print(TP, '|', FN)
-    print(FP, '|', TN)
-
-    print('\n'*3)
-
-    testResult2 = someTree.testBatch(testData)
-
-    numMiss = 0
-    numHit = 0
-    FN = TN = TP = FP = 0
-
-    for x in testResult2:
-
-        if(x[2]):
-            numHit+=1
-            if(x[0]):
-                TP+=1
-            else:
-                TN+=1
-        else:
-            numMiss+=1
-            if(x[0]):
-                FP+=1
-            else:
-                FN+=1
-
-    print('Test Error Percent:', (numMiss*100)/(numMiss+numHit))
-
-
-    '''
-        PV ->
-    AV    _P_ _N_
-    | |P[TP][FN]      2|1
-     V |N[FP][TN]      3|4
-    '''
-
-    print(TP, '|', FN)
-    print(FP, '|', TN)
-    
 
 # function to create the decision tree and find the training and test errors
-def trainAndTest(trainingData, x, testData):
+def trainAndTest(trainingData, x, testData, makeConfusion):
 
+    # create the decision tree
     someTree = dt.decisionTree(trainingData, x)
-
+    
+    # train the tree
     someTree.train()
 
     print(str(someTree))
 
+    # find training error and make confusion matrix if needed
     testResult = someTree.testBatch(trainingData)
+    print('Results for Training Data: ')
+    findResults(testResult, makeConfusion)
+    
+    print('\n'*3)
+    
+    # find test error and make confusion matrix if needed
+    testResult2 = someTree.testBatch(testData)
+    print('Results for Test Data: ')
+    findResults(testResult2, makeConfusion)
 
     
